88060aa398f0f5392b642f3a1293989331919646
amart251 <amartinez251@miners.utep.edu>
2023-08-06 21:53:25 -0600

    interrupts

--- projectMain/button.c
+++ projectMain/button.c
@@ -19,4 +19,39 @@ void button_init(void)
     P2DIR &= ~BIT3;  // Set P2.3 as input
     P2REN |= BIT3;  // Enable pull-up resistor on P2.3
     P2OUT |= BIT3;  // Set pull-up resistor to pull-up mode
+
+    P2IE |= (BIT0 | BIT1 | BIT2 | BIT3); // Enable interrupt on P2.0 - P2.3
+    P2IES |= (BIT0 | BIT1 | BIT2 | BIT3); // Set interrupt edge select to falling edge
+    P2IFG &= ~(BIT0 | BIT1 | BIT2 | BIT3); // Clear interrupt flags
+
+    __enable_interrupt(); // Enable global interrupts
+}
+
+// Interrupt service routine for button press
+#pragma vector=PORT2_VECTOR
+__interrupt void button_isr(void)
+{
+    if (P2IFG & BIT0) // Check if P2.0 interrupt flag is set
+    {
+        // Your code to handle the button press event on P2.0
+        P2IFG &= ~BIT0; // Clear interrupt flag
+    }
+
+    if (P2IFG & BIT1) // Check if P2.1 interrupt flag is set
+    {
+        // Your code to handle the button press event on P2.1
+        P2IFG &= ~BIT1; // Clear interrupt flag
+    }
+
+    if (P2IFG & BIT2) // Check if P2.2 interrupt flag is set
+    {
+        // Your code to handle the button press event on P2.2
+        P2IFG &= ~BIT2; // Clear interrupt flag
+    }
+
+    if (P2IFG & BIT3) // Check if P2.3 interrupt flag is set
+    {
+        // Your code to handle the button press event on P2.3
+        P2IFG &= ~BIT3; // Clear interrupt flag
+    }
 }
