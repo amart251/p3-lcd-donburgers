13d3bfbe9edca26ea71003abc9da4abc5b3362ff
amart251 <amartinez251@miners.utep.edu>
2023-08-06 21:28:55 -0600

    fj

--- projectMain/main.c
+++ projectMain/main.c
@@ -42,7 +42,7 @@ int main(void)
                 stopBuzzer();
                 delay_ms(250 / noteDurations2[i]);  // Short delay between notes
             }
-        }
+        }/*
         else if ((P2IN & BIT3) == 0) {  // If button on P2.1 is pressed
             // Play melody 3
             for (int i = 0; melody3[i] != -1; i++) {
@@ -60,7 +60,7 @@ int main(void)
                 stopBuzzer();
                 delay_ms(250 / noteDurations4[i]);  // Short delay between notes
             }
-        }
+        }*/
     }
 
     return 0;
